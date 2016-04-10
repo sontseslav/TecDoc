d559f51bc89d71da4fb9c14c516c4fdcb941c5ca
coder <s.khvalinsky@gmail.com>
2016-04-10 22:18:41 +0300

    commit 1

--- src/concur/dbconctest/ParallelSummarise.java
+++ src/concur/dbconctest/ParallelSummarise.java
@@ -7,38 +7,65 @@ import java.util.concurrent.*;
  * Created by coder on 10.04.16.
  */
 public class ParallelSummarise extends Thread{
-    private class Task implements Runnable{
+    private class Pair{
+        private int sum;
+        public Pair(int val1,int val2){
+            this.sum = val1 + val2;
+        }
+
+        public long getSum(){
+            return this.sum;
+        }
+    }
+    private class TaskInput implements Runnable{
         @Override
-        public synchronized void run(){
+        public void run(){
             int val1,val2;
             try{
                 while(rs.next()){
-                    synchronized (this) {
+
                         val1 = rs.getInt(1);
                         val2 = rs.getInt(2);
-                    }
-                    synchronized (this) {
-                        ps.setNull(1, Types.INTEGER);
+                        queue.put(new Pair(val1, val2));
+
+                        /*ps.setNull(1, Types.INTEGER);
                         ps.setLong(2, val1 + val2);
                         ps.setBoolean(3, false);
                         ps.executeUpdate();
-                    }
-                    synchronized (this) {
                         if((rowsSet % 5000) == 0 && rowsSet !=0){
                             System.out.println(Thread.currentThread().getName()+" : "+rowsSet);
                             //conn.commit();
                         }
-                        rowsSet++;
-                    }
+                        rowsSet++;*/
                 }
-                isNext = false;
-            }catch (SQLException e){e.printStackTrace();}
+                //isNext = false;
+            }catch (SQLException | InterruptedException e){e.printStackTrace();}
         }
     }
-    volatile private PreparedStatement ps;
+    private class TaskOutput implements Runnable{
+        @Override
+        public void run(){
+            try{
+                ps.setNull(1,Types.INTEGER);
+
+                    ps.setLong(2, queue.take().getSum());
+
+                ps.setBoolean(3, false);
+                ps.executeUpdate();
+                if((rowsSet % 5000) == 0 && rowsSet !=0){
+                    System.out.println(Thread.currentThread().getName()+" : "+rowsSet);
+                    //conn.commit();
+                }
+                rowsSet++;
+                if(queue.size()==0){isNext = false;}
+            }catch (SQLException | InterruptedException e){e.printStackTrace();}
+        }
+    }
+    private PreparedStatement ps;
     volatile private ResultSet rs;
     private Connection conn;
     volatile private int rowsSet;
+    private BlockingQueue<Pair> queue;
     private final int corePoolSize = 5;
     private final int maxPoolSize = 10;
     private final long keepAliveTime = 5000;
@@ -52,13 +79,15 @@ public class ParallelSummarise extends Thread{
 
     @Override
     public void run(){
+        queue = new ArrayBlockingQueue<>(5000);
         ExecutorService threadPoolExecutor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime,
                 TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
         for (int i = 0; i<maxPoolSize;i++){
-            threadPoolExecutor.execute(new Task());
+            threadPoolExecutor.execute(new TaskInput());
+            threadPoolExecutor.execute(new TaskOutput());
         }
         //ExecutorService threadPoolExecutor = Executors.newSingleThreadExecutor();
-        //threadPoolExecutor.execute(new Task());
+        //threadPoolExecutor.execute(new TaskInput());
         while(isNext){
             try{
                 Thread.sleep(5000);
